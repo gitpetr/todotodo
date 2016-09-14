04c48ec6791b8817be71548de1fb226590535351
gitpetr <pkhilenko@yandex.ru>
2016-09-14 10:35:40 +0600

    completed

---
+++ db/migrate/20160914034159_create_notes.rb
@@ -0,0 +1,12 @@
+class CreateNotes < ActiveRecord::Migration
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.text :content
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+    add_index :notes, :user_id
+  end
+end
