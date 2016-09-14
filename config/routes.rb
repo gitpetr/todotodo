f709138b0f242f130619264508105938b558a648
gitpetr <pkhilenko@yandex.ru>
2016-09-14 10:34:53 +0600

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :notes
+  authenticated :user do
+    root "notes#index", as: "authenticated_root"
+  end
+  root "welcome#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
